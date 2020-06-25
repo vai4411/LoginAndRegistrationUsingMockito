879f1a0fcaca25ab7671992a7806f3ec38b78262
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-25 20:40:13 +0530

    Initial Commit

---
+++ src/main/java/com/bl/loginmock/LoginMockApplication.java
@@ -0,0 +1,13 @@
+package com.bl.loginmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bl/loginmock/LoginMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.loginmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

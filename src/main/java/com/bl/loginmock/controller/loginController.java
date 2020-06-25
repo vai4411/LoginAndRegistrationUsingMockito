873751601bc4b1ebdc71024a8fd280d08b5db6a4
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-25 22:10:16 +0530

    ADDED: Used Login Controller For Manage Login Request

---
+++ src/main/java/com/bl/loginmock/controller/loginController.java
@@ -0,0 +1,12 @@
+package com.bl.loginmock.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/login")
+public class loginController {
+
+
+}

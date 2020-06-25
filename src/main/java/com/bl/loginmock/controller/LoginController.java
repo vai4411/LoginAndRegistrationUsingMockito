007cef742a06342de6342ec8be85af57930042dd
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-26 00:15:44 +0530

    ADDED: Used Login And User Model For Passing Data In Object Format

--- src/main/java/com/bl/loginmock/controller/LoginController.java
+++ src/main/java/com/bl/loginmock/controller/LoginController.java
@@ -1,12 +1,13 @@
 package com.bl.loginmock.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bl.loginmock.model.Login;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/login")
-public class loginController {
-
+public class LoginController {
 
 }
---
+++ src/main/java/com/bl/loginmock/model/User.java
@@ -0,0 +1,35 @@
+package com.bl.loginmock.model;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private int id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        System.out.println("username: " + username);
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
